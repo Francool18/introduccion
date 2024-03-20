eb5b6c044bdd35aa37941fb59f341e461200dc67
Francool18 <francisco.montesdeoca2605@gmail.com>
2024-03-20 09:14:12 -0300

    puse comentarios

--- imprimir.php
+++ imprimir.php
@@ -2,10 +2,14 @@
 <head></head>
 <h1>mi segundo archivo en php </h1>
 <?php
-echo<<<END
+//el end tiene que estar todo junto
+
+echo <<<END
 En un mundo contectado,
 donde cada segundo se envia millones de correos
 electonicos...
 END;    ?>
+/* no puedo olvidarme 
+cerrar el php */
 </body>
 </html>
