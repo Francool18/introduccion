75f65f7881584d13abc71471da05243d0d226d29
Francool18 <francisco.montesdeoca2605@gmail.com>
2024-03-20 08:59:17 -0300

    es un nuevo archivo con END

---
+++ imprimir.php
@@ -0,0 +1,11 @@
+<html>
+<head></head>
+<h1>mi segundo archivo en php </h1>
+<?php
+echo<<<END
+En un mundo contectado,
+donde cada segundo se envia millones de correos
+electonicos...
+END;    ?>
+</body>
+</html>
