e1308d01bfdf9f653957aacf46b2bdc44321a24f
francool18 <francisco.montesdeoca2605@gmail.com>
2024-05-29 09:23:55 -0300

    termine el tabajo extra

--- microcuento.php
+++ microcuento.php
@@ -1,25 +1,33 @@
 <!DOCTYPE html>
 <head>
+    <link rel ="stylesheet" href="estilomicrocuento.css">
 </head>
 <body>
+    <u><h1> Verificación del cuento </h1></u>
 <?php 
 $cuento="En un rincón olvidado de la ciudad, vive un anciano relojero cuya tienda es un museo de tiempo. 
 Destaca un reloj especial que marca siempre las doce campanadas, considerado el reloj del destino. 
 Una noche, una joven desesperada entra buscando dirección. El anciano le muestra el reloj del destino y ella, tras girar las manecillas, desaparece en una luz brillante. 
 Al día siguiente, la ciudad despierta con la leyenda de la joven que encontró su destino en el tiempo, dejando al anciano relojero satisfecho con su obra.";
+?>
+<div id="cuento">
+<?php
 echo $cuento;
+?>
+</div>
+<?php
 echo "<br>";
 echo "<br>";
 $relato=explode(" ",$cuento);
 $cantpal=count($relato);
 $cantlet=strlen($cuento);
-echo "el texto tiene ".$cantpal." palabras y ".$cantlet."caracteres";
+echo "el texto tiene ".$cantpal." palabras y ".$cantlet." caracteres";
 echo "<br>";
-// hacer con un if la cantidad de palabray y letras
-if($cantpal<=100 && $cantlet<=700){
-    echo "El cuento cumple con la condición";
+?>
+<?php if($cantpal<=100 && $cantlet<=700){ 
+    echo '<p style ="color: green"> El cuento cumple con la condición</p>';
 }else{
-    echo "No cumple con las condiciones";
+    echo '<p style="color: red">"No cumple con las condiciones"</p>';
 }
 ?>
 </body>
