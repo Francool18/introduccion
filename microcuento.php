f3aa07b7ecc39950c3ba53cddd62d20e31061f7f
francool18 <francisco.montesdeoca2605@gmail.com>
2024-05-29 07:28:43 -0300

    termine el trabajo

--- microcuento.php
+++ microcuento.php
@@ -3,13 +3,24 @@
 </head>
 <body>
 <?php 
-$cuento="En un rincón olvidado de la ciudad, vive un anciano relojero cuya tienda es un museo de tiempo. Destaca un reloj especial que marca siempre las doce campanadas, considerado el reloj del destino. Una noche, una joven desesperada entra buscando dirección. El anciano le muestra el reloj del destino y ella, tras girar las manecillas, desaparece en una luz brillante. Al día siguiente, la ciudad despierta con la leyenda de la joven que encontró su destino en el tiempo, dejando al anciano relojero satisfecho con su obra.";
-
+$cuento="En un rincón olvidado de la ciudad, vive un anciano relojero cuya tienda es un museo de tiempo. 
+Destaca un reloj especial que marca siempre las doce campanadas, considerado el reloj del destino. 
+Una noche, una joven desesperada entra buscando dirección. El anciano le muestra el reloj del destino y ella, tras girar las manecillas, desaparece en una luz brillante. 
+Al día siguiente, la ciudad despierta con la leyenda de la joven que encontró su destino en el tiempo, dejando al anciano relojero satisfecho con su obra.";
+echo $cuento;
+echo "<br>";
+echo "<br>";
 $relato=explode(" ",$cuento);
 $cantpal=count($relato);
 $cantlet=strlen($cuento);
 echo "el texto tiene ".$cantpal." palabras y ".$cantlet."caracteres";
+echo "<br>";
 // hacer con un if la cantidad de palabray y letras
+if($cantpal<=100 && $cantlet<=700){
+    echo "El cuento cumple con la condición";
+}else{
+    echo "No cumple con las condiciones";
+}
 ?>
 </body>
 </html>
